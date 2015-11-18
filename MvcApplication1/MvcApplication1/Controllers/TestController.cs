88ee62090d0cd60e39307b086e57106e14854000
cnwbl1 <bliu@wynyardgroup.com>
2015-11-18 13:44:08 +1300

    first commit

---
+++ MvcApplication1/MvcApplication1/Controllers/TestController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class Customer
+    {
+        public string CustomerName { get; set; }
+        public string Address { get; set; }
+
+        public override string ToString()
+        {
+            return this.CustomerName + "|" + this.Address;
+        }
+    }
+    public class TestController : Controller
+    {
+       
+
+        public Customer GetCustomer()
+        {
+            var c = new Customer {CustomerName = "Customer 1", Address = "Address1"};
+            return c;
+        }
+    }
+}
