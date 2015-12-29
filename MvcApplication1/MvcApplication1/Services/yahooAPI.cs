02b340c3e82ac3e5e7213ac5d2e1ee0ea9963d3c
boChuck <bo.rubyxing@gmail.com>
2015-12-29 17:01:31 +1300

    METHOD UPDATED

--- MvcApplication1/MvcApplication1/Services/yahooAPI.cs
+++ MvcApplication1/MvcApplication1/Services/yahooAPI.cs
@@ -9,6 +9,9 @@ namespace MvcApplication1.Services
 {
     public class yahooAPI
     {
+        private const string BASE_URL = "https://maps.googleapis.com/maps/api/geocode/json?address=";
+        private const string KEY = "key=AIzaSyDrmXBif4-NYHjuIEx2mrAIJ2ZwiUFsZ98";
+
         public static string FetchGoogleData(string address)
         {
             var r =
