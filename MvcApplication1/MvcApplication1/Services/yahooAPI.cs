e68d31607cd1f096a34a7f582829665021b2b4e7
boChuck <bo.rubyxing@gmail.com>
2015-12-29 16:57:30 +1300

    method updated

--- MvcApplication1/MvcApplication1/Services/yahooAPI.cs
+++ MvcApplication1/MvcApplication1/Services/yahooAPI.cs
@@ -9,12 +9,12 @@ namespace MvcApplication1.Services
 {
     public class yahooAPI
     {
-        public static string FetchGoogleData()
+        public static string FetchGoogleData(string address)
         {
             var r =
                 WebRequest.Create(
                     new Uri(
-                        "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22AAPL%22)%0A%09%09&format=json&diagnostics=true&env=http%3A%2F%2Fdatatables.org%2Falltables.env&callback="));
+                        "https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "key=AIzaSyDrmXBif4-NYHjuIEx2mrAIJ2ZwiUFsZ98"));
 
             return new StreamReader(r.GetResponse().GetResponseStream()).ReadToEnd();
         }
